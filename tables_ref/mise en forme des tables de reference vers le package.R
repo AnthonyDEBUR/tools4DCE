90356925e02eb88f8f0fc7ec1576927878e77661
AnthonyDEBUR <anthony.deburghrave@eptb-vilaine.fr>
2021-01-29 20:08:17 +0100

    correction table seuils AEP

--- tables_ref/mise en forme des tables de reference vers le package.R
+++ tables_ref/mise en forme des tables de reference vers le package.R
@@ -3,34 +3,47 @@ library(readxl)
 library(devtools)
 
 # base des seuils par paramètre
-base_seuils <- read_xlsx("tables_ref/base_seuils.xlsx", col_types =  c(rep("text",5),rep("numeric",2), rep("text",5)))
-save(base_seuils, file="data/base_seuils.RData")
+base_seuils <-
+  read_xlsx("tables_ref/base_seuils.xlsx", col_types =  c(rep("text", 5), rep("numeric", 2), rep("text", 5)))
+save(base_seuils, file = "data/base_seuils.RData")
 
 
 # couleur des classes de qualité
 couleurs_classes <- read.csv2("tables_ref/couleurs_classes.csv")
-save(couleurs_classes, file="data/couleurs_classes.RData")
+save(couleurs_classes, file = "data/couleurs_classes.RData")
 
 
 # ordre des facteurs de qualité
-ordre_facteurs_qualite <- read.csv2("tables_ref/ordre_facteurs_qualite.csv")
-names(ordre_facteurs_qualite)<-"CLASSE"
-save(ordre_facteurs_qualite, file="data/ordre_facteurs_qualite.RData")
+ordre_facteurs_qualite <-
+  read.csv2("tables_ref/ordre_facteurs_qualite.csv")
+names(ordre_facteurs_qualite) <- "CLASSE"
+save(ordre_facteurs_qualite, file = "data/ordre_facteurs_qualite.RData")
 
 # telechargement du referentiel unites sandre
-download.file("https://api.sandre.eaufrance.fr/referentiels/v1/urf.csv?compress=true", "unites.csv.gz", mode="wb", cacheOK = T)
-unites_sandre<-read_delim("unites.csv.gz",delim=";", skip=1)
+download.file(
+  "https://api.sandre.eaufrance.fr/referentiels/v1/urf.csv?compress=true",
+  "unites.csv.gz",
+  mode = "wb",
+  cacheOK = T
+)
+unites_sandre <- read_delim("unites.csv.gz", delim = ";", skip = 1)
 file.remove("unites.csv.gz")
-save(unites_sandre, file="data/unites_sandre.RData")
+save(unites_sandre, file = "data/unites_sandre.RData")
 
 # telechargement du referentiel paramètres sandre
-download.file("https://api.sandre.eaufrance.fr/referentiels/v1/par.csv?outputSchema=SANDREv4&compress=true", "param.csv.gz", mode="wb", cacheOK = T)
-parametres_sandre<-read_delim("param.csv.gz",delim=";", skip=1)
-parametres_sandre$CdParametre<-as.character(parametres_sandre$CdParametre)
+download.file(
+  "https://api.sandre.eaufrance.fr/referentiels/v1/par.csv?outputSchema=SANDREv4&compress=true",
+  "param.csv.gz",
+  mode = "wb",
+  cacheOK = T
+)
+parametres_sandre <- read_delim("param.csv.gz", delim = ";", skip = 1)
+parametres_sandre$CdParametre <-
+  as.character(parametres_sandre$CdParametre)
 # suppression des colonnes remplies de NA
-parametres_sandre<-Filter(function(x) !all(is.na(x)), parametres_sandre)
+parametres_sandre <-
+  Filter(function(x)
+    ! all(is.na(x)), parametres_sandre)
 file.remove("param.csv.gz")
 # usethis::use_data(parametres_sandre, overwrite=T)
-save(parametres_sandre, file="data/parametres_sandre.RData")
-
-
+save(parametres_sandre, file = "data/parametres_sandre.RData")
