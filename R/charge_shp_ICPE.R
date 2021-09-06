aacd1680abb77ddba8469234f6af4237c1a86dc6
AnthonyDEBUR <anthony.deburghrave@eptb-vilaine.fr>
2021-09-06 15:57:39 +0200

    maj

--- R/charge_shp_ICPE.R
+++ R/charge_shp_ICPE.R
@@ -8,7 +8,7 @@
 #' @return la fonction renvoie un objet sf avec l'emprise des SAGE sélectionnés et les attributs correspondants
 #' @examples Vilaine<-charge_shp_SAGE(nom_sage="Vilaine")
 #' @export
-charge_shp_ICPE <- function(nom_sage = NULL, crs = 2154) {
+charge_shp_ICPE <- function(crs = 2154, shp_emprise = NULL) {
   # on charge le shp des SAGE de France à partir de https://www.georisques.gouv.fr/donnees/bases-de-donnees/installations-industrielles
   url <- "https://mapsref.brgm.fr/wxs/georisques/georisques_dl"
 
@@ -27,16 +27,28 @@ charge_shp_ICPE <- function(nom_sage = NULL, crs = 2154) {
     ) %>%
     build_url()
 
-  bel_regions <- read_sf(sf_prov, crs = 4326)
+  tmp<-tempfile()
+  download.file(sf_prov, destfile = tmp, mode="wb")
 
-  # On se limite aux SAGE sélectionnés
-  if (!is.null(nom_sage)) {
-    bel_regions <- bel_regions %>% subset(NomZone %in% nom_sage)
+  tmp2<-tempdir()
+
+  unzip(tmp, exdir=tmp2)
+
+
+
+  bel_regions <- read_sf(paste0(tmp2,"\\InstallationsClassees.shp"),crs = 4326)
+
+  # sélection des STEP dans l'emprise de découpe
+  if (!is.null(shp_emprise)) {
+    # on reprojette dans en Lambert 93 avant decoupage
+    bel_regions <- st_transform(bel_regions, crs = 2154)
+    shp_emprise <- st_transform(shp_emprise, crs = 2154)
+
+    # on découpe par rapport à l'emprise de l'objet shp_emprise
+    bel_regions <- bel_regions[shp_emprise, ]
   }
 
-  # on reprojette dans le crs d'entrée
+  # on projette dans le crs de sortie
   bel_regions <- st_transform(bel_regions, crs = crs)
-
-
   return(bel_regions)
 }
