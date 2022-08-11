e292737617cda9c891e8fd9afadcd7940bf9ee09
AnthonyDEBUR <anthony.deburghrave@eptb-vilaine.fr>
2022-08-11 16:28:56 +0200

    maj

--- R/ajoute_nom_cdqualana.R
+++ R/ajoute_nom_cdqualana.R
@@ -3,20 +3,20 @@
 #' fonction pour ajouter le nom du code qualification de l'analyse sandre à une data frame
 #'
 #' @param x data.frame avec le code paramètre
-#' @param col_qualif : nom de la colonne avec le code qualification Valeur par défaut : CqQualAna
+#' @param col_qualif : nom de la colonne avec le code qualification Valeur par défaut : CdQualAna
 #'
 #'
 #' @return la fonction le data.frame complété de la colonne RqAna
 #'
-#' @examples test<-data.frame(CqQualAna=c("1", "10"))
-#' @examples ajoute_nom_cdqualana(test, col_qualif="CqQualAna")
+#' @examples test<-data.frame(CdQualAna=c("1", "10"))
+#' @examples ajoute_nom_cdqualana(test, col_qualif="CdQualAna")
 #' @export
 
 ajoute_nom_cdqualana <-
   function(x,
-           col_qualif="CqQualAna") {
+           col_qualif="CdQualAna") {
 
-    data("CqQualAna", envir=environment())
+    data("CdQualAna", envir=environment())
 
-    return(left_join(x, CqQualAna, by = setNames("CqQualAna", col_qualif)))
+    return(left_join(x, CdQualAna, by = setNames("CdQualAna", col_qualif)))
   }
